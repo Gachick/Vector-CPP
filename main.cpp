7d6d7de24b6e716a9aa8f7a2cbc016e08584a90b
Gachick <maksdayt@gmail.com>
2023-12-09 14:06:22 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <complex>
+#include "vector.h"
+#include <vector>
+#include <random>
+#include <string>
+
+void test_leak()
+{
+    vector<double> test(10000000);
+}
+
+vector<double> test_move()
+{
+    vector<double> test(10000000);
+    return test;
+}
+
+vector<double> make_vec()
+{
+    vector<double> move{3, 4, 5};
+    return move;
+}
+
+int main()
+{
+    test_leak();
+    vector<double> move{test_move()};
+
+    vector<double> a;
+    std::cout << "Default constructor " << a << "\n";
+
+    vector<int> b{1, 2, 3};
+    std::cout << "Initializer list " << b << "\n";
+
+    vector<double> c(5);
+    std::cout << "Set size default argument " << c << "\n";
+
+    vector<double> d(3, 1.2);
+    std::cout << "Set size set argument " << d << "\n";
+
+    vector<double> copy{1.4, 2.72, 3.14};
+    vector<double> e{copy};
+    std::cout << "Copy constructor " << e << "\n";
+    vector<double> f;
+    f = e;
+    std::cout << "Copy operator " << f << "\n";
+
+    vector<double> g{make_vec()};
+    std::cout << "Copy constructor " << g << "\n";
+    vector<double> j;
+    j = make_vec();
+    std::cout << "Copy operator " << j << "\n";
+
+    vector<std::complex<double>> k{std::complex<double>{1, 2}, std::complex<double>{3, 4}};
+    std::cout << "Complex type " << k << "\n";
+
+    vector<double> l;
+    l.push_back(6.7);
+    std::cout << "Push back into empty vector " << l << "\n";
+
+    vector<double> p{8.9};
+    p.push_back(3.2);
+    p.push_back(4.5);
+    std::cout << "Push back into non-empty vector " << p << "\n";
+
+    // vector<int> v(-1);
+    //  std::vector<int>(-1);
+    // while (true)
+    // {
+    //     vector<double> v(100000);
+    //     for (int i = 0; i < 100000; ++i)
+    //     {
+    //         v[i] = i;
+    //     }
+    //     std::cout << "Loop";
+    // }
+
+    vector<std::string> type_test;
+    // type_test.push_back(1);
+    vector<vector<int>> vofv(5, vector<int>(5, 5));
+    vofv[1][2] = 13;
+    std::cout << vofv;
+}
---
+++ vector.h
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <memory>
+#include <iostream>
+
+//------------------------------------------------------------------------------
+
+template <typename T>
+class allocator
+{
+public:
+    T *allocate(int n) { return static_cast<T *>(::operator new(n * sizeof(T))); }
+    void deallocate(T *p) { ::operator delete(p); }
+    void construct(T *p, const T &v) { new (p) T{v}; }
+    void destroy(T *p) { p->~T(); }
+};
+
+//------------------------------------------------------------------------------
+
+struct Range_error : std::out_of_range
+{
+    int index;
+    Range_error(int i) : std::out_of_range{"Range error "}, index{i} {}
+};
+
+//------------------------------------------------------------------------------
+
+template <typename T, typename A = allocator<T>>
+class vector
+{
+    A alloc;
+    int sz;
+    T *elem;
+    int space;
+
+public:
+    vector() : sz{0}, elem{nullptr}, space{0} {};
+    explicit vector(int s, T def = T());
+    vector(std::initializer_list<T> lst);
+
+    vector(const vector &);
+    vector &operator=(const vector &);
+
+    vector(vector &&);
+    vector &operator=(vector &&);
+
+    ~vector() { alloc.deallocate(elem); };
+
+    T &operator[](int n) { return elem[n]; };
+    const T &operator[](int n) const { return elem[n]; };
+
+    T &at(int n);
+    const T &at(int n) const;
+
+    int size() const { return sz; };
+    int capacity() const { return space; };
+
+    void resize(int newsize, T def = T());
+    void push_back(const T &d);
+    void reserve(int newalloc);
+
+    friend std::ostream &operator<<(std::ostream &os, const vector &vec)
+    {
+        os << "[";
+        for (int i = 0; i < vec.size(); ++i)
+            os << vec[i] << ", ";
+        os << "\b\b]";
+        return os;
+    }
+};
+//------------------------------------------------------------------------------
+
+template <typename T, typename A>
+vector<T, A>::vector(int s, T def) : sz{s}, elem{nullptr}, space{s}
+{
+    if (s < 0)
+    {
+        throw std::length_error("Can't create a vector of negative lenght");
+    }
+    elem = alloc.allocate(s);
+    for (T *p = elem; p != elem + s; ++p)
+        alloc.construct(p, def);
+}
+
+template <typename T, typename A>
+vector<T, A>::vector(std::initializer_list<T> lst) : sz{static_cast<int>(lst.size())}, elem{nullptr}, space{sz}
+{
+    elem = alloc.allocate(sz);
+
+    T *p = elem;
+    for (T item : lst)
+    {
+        alloc.construct(p, item);
+        ++p;
+    }
+}
+
+template <typename T, typename A>
+vector<T, A>::vector(const vector<T, A> &a)
+    : sz{a.size()}, elem{alloc.allocate(a.sz)}, space{a.size()}
+{
+    std::copy(a.elem, a.elem + a.sz, elem); // PROBLEMS
+}
+
+template <typename T, typename A>
+vector<T, A>::vector(vector<T, A> &&a)
+    : sz{a.sz}, elem{a.elem}, space{a.sz}
+{
+    a.sz = 0;
+    a.elem = nullptr;
+}
+
+template <typename T, typename A>
+vector<T, A> &vector<T, A>::operator=(vector<T, A> &&a)
+{
+    alloc.deallocate(elem);
+    elem = a.elem;
+    sz = a.sz;
+    a.elem = nullptr;
+    a.sz = 0;
+    return *this;
+}
+
+template <typename T, typename A>
+vector<T, A> &vector<T, A>::operator=(const vector<T, A> &a)
+{
+    if (this == &a)
+        return *this;
+
+    if (a.sz <= space)
+    {
+        for (int i = 0; i < a.sz; ++i)
+            elem[i] = a.elem[i];
+        sz = a.sz;
+        return *this; // check alements after size
+    }
+
+    std::unique_ptr<T[]> p(alloc.allocate(a.sz));
+    for (int i = 0; i < a.sz; ++i)
+        p[i] = a.elem[i];
+    alloc.deallocate(elem);
+    space = sz = a.sz;
+    elem = p.release();
+    return *this;
+}
+
+template <typename T, typename A>
+T &vector<T, A>::at(int n)
+{
+    if (n < 0 || sz <= n)
+        throw Range_error(n);
+    return elem[n];
+}
+
+template <typename T, typename A>
+void vector<T, A>::reserve(int newalloc)
+{
+    if (newalloc <= space)
+        return;
+    std::unique_ptr<T[]> p(alloc.allocate(newalloc));
+    for (int i = 0; i < sz; ++i)
+        alloc.construct(&p[i], elem[i]);
+    for (int i = 0; i < sz; ++i)
+        alloc.destroy(&elem[i]);
+    alloc.deallocate(elem);
+    elem = p.release();
+    space = newalloc;
+}
+
+template <typename T, typename A>
+void vector<T, A>::push_back(const T &val)
+{
+    if (space == 0)
+        reserve(8);
+    else if (sz == space)
+        reserve(2 * space);
+    alloc.construct(&elem[sz], val);
+    ++sz;
+}
+
+template <typename T, typename A>
+void vector<T, A>::resize(int newsize, T val)
+{
+    reserve(newsize);
+    for (int i = sz; i < newsize; ++i)
+        alloc.construct(&elem[i], val);
+    for (int i = newsize; i < sz; ++i)
+        alloc.destroy(&elem[i]);
+    sz = newsize;
+}
+
+//------------------------------------------------------------------------------
